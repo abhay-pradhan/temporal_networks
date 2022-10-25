e34b577be03765338729e983667021718eabe900
Abhay Pradhan <abhay.pradhan@ihsmarkit.com>
2022-10-25 19:12:48 +0100

    Random pick of edges and vertex shuffle

--- preferentialattachment.py
+++ preferentialattachment.py
@@ -1,11 +1,10 @@
-import configparser
-
 import igraph
 import pandas as pd
+import configparser
 import pycountry
+import random
 import ray
 from matplotlib.colors import LinearSegmentedColormap
-from progress.spinner import MoonSpinner
 
 
 def load_config(config_file):
@@ -39,17 +38,17 @@ def get_country(name):
         code = pycountry.countries.get(name=name).alpha_2
         return code
     except:
-        return name
+        tokens = str.split(' ')
+        return tokens[0][0] + tokens[-1][0]
 
 
-def get_g7_brics_country(name):
-    try:
-        code = None
-        if (name in G7) or (name in BRICS):
-            code = pycountry.countries.get(name=name).alpha_2
-        return code
-    except:
-        return name
+def get_sz(name):
+    if name in G7:
+        return 50
+    elif name in BRICS:
+        return 50
+    else:
+        return 30
 
 
 def load_dataframe(data_file, ts_col, cols_to_load, end_timestep):
@@ -60,24 +59,6 @@ def load_dataframe(data_file, ts_col, cols_to_load, end_timestep):
     ret_df = df[df[ts_col] < end_timestep]
     tt = max(ret_df[ts_col])
     return ret_df, tt
-
-
-def filter_dataframe(df, ts_col, from_countries, num_to_pick):
-    drop_df = df.drop_duplicates(subset=['FromCty', 'ToCty'], keep='first')
-    drop_df = drop_df[drop_df['FromCty'].isin(from_countries)]
-    drop_df = drop_df.reset_index(drop=True)
-    drop_df[ts_col] = drop_df.index
-    grouped_df = drop_df.groupby(['FromCty'], as_index=False, sort=False).agg(lambda to: list(to))
-    grouped_df = grouped_df[['FromCty', 'ToCty', 'timestep']]
-    grouped_df['timestep'] = grouped_df.index
-    grouped_df['ToCty'] = grouped_df['ToCty'].map(lambda x: x[0:num_to_pick])
-    grouped_df = grouped_df.apply(pd.Series.explode).reset_index()
-
-    ret_df = grouped_df[['FromCty', 'ToCty', 'timestep']]
-    ret_df['timestep'] = ret_df.index
-
-    print(f'filtered dataframe size is {len(ret_df.index)}')
-    return ret_df
 
 
 def delete_edges(g, t):
@@ -93,16 +74,11 @@ def delete_vertices(g):
     return g_copy
 
 
-cmap2 = LinearSegmentedColormap.from_list("edge_cmap", ["lightblue", "midnightblue"])
-
-
-def get_sz(name):
-    if name in G7:
-        return 60
-    elif name in BRICS:
-        return 40
-    else:
-        return 20
+# this takes 2 arguments, the 1st argument is simply the name, the second one is a tuple of colors so that a 
+# particular betweeness will reside within the boundary of the 2 colors specified
+# Pick the colors from this URL below
+# https://www.w3schools.com/colors/colors_x11.asp
+cmap2 = LinearSegmentedColormap.from_list("edge_cmap", ["cornflowerblue", "darkslateblue"])
 
 
 def format_graph(graph, country_label: False):
@@ -112,46 +88,46 @@ def format_graph(graph, country_label: False):
         g_copy.vs['label'] = [get_country(name) for name in g_copy.vs['name']]
 
     g_copy.vs['color'] = [get_color(x) for x in g_copy.vs['name']]
-
-    #vs_betweenness = g_copy.betweenness()
-    #if vs_betweenness:
-    #    g_copy.vs["size"]  = igraph.rescale(vs_betweenness, (30, 80))
     g_copy.vs['size'] = [get_sz(name) for name in g_copy.vs['name']]
 
     edge_betweenness = g_copy.edge_betweenness()
     if edge_betweenness:
         scaled_edge_betweenness = igraph.rescale(edge_betweenness, clamp=True)
         g_copy.es["color"] = [cmap2(betweenness) for betweenness in scaled_edge_betweenness]
-        g_copy.es["width"] = igraph.rescale(edge_betweenness, (0.1, 1.0))
+        g_copy.es["width"] = igraph.rescale(edge_betweenness, (0.5, 1.0))
 
     return g_copy
 
 
+def filter_dataframe(df, ts_col, from_countries, num_to_pick):
+    drop_df = df.drop_duplicates(subset=['FromCty', 'ToCty'], keep='first')
+    drop_df = drop_df[drop_df['FromCty'].isin(from_countries)]
+    drop_df = drop_df.reset_index(drop=True)
+    drop_df[ts_col] = drop_df.index
+    grouped_df = drop_df.groupby(['FromCty'], as_index=False, sort=False).agg(lambda to: list(to))
+    grouped_df = grouped_df[['FromCty', 'ToCty', 'timestep']]
+    grouped_df['timestep'] = grouped_df.index
+    grouped_df['ToCty'] = grouped_df['ToCty'].map(lambda x: random.sample(x, num_to_pick))
+    grouped_df = grouped_df.apply(pd.Series.explode).reset_index()
+
+    # now randomly shuffling the rows
+    grouped_df = grouped_df.sample(frac=1).reset_index(drop=True)
+
+    ret_df = grouped_df[['FromCty', 'ToCty', 'timestep']]
+    ret_df['timestep'] = ret_df.index
+
+    print(f'filtered dataframe size is {len(ret_df.index)}')
+    return ret_df
+
+
+#Not used
 def bucket_df(df, ts_col, bucket_sz):
     interval = pd.interval_range(0, df[ts_col].iat[-1], bucket_sz)
     bucketed = df.groupby(pd.cut(df[ts_col], bins=interval, right=True)).agg(lambda x: list(x))
     return bucketed
 
 
-def construct_graph(layout, bucketed_df, ts_col, country_label):
-    original_graph = igraph.Graph.DataFrame(bucketed_df, directed=False)
-    old_layout = original_graph.layout(layout)
-    n = 1
-    with MoonSpinner('Generating graph...') as bar:
-        for idx, row in bucketed_df.iterrows():
-            last_ts_per_bucket = row[ts_col][-1]
-            g = delete_edges(original_graph, last_ts_per_bucket)
-            new_layout = g.layout(layout, seed=old_layout)
-            gg = format_graph(delete_vertices(g), country_label)
-            tgt = f'{output_folder}/example{n}.png'
-            igraph.plot(gg, layout=new_layout, target=tgt, bbox=(1600,1200))
-            old_layout = new_layout.copy()
-            n += 1
-            bar.next()
-
-
-def construct_filtered_graph(layout, df, country_label):
-    original_graph = igraph.Graph.DataFrame(df, directed=False)
+def construct_graph(layout, df, original_df, country_label):
     old_layout = original_graph.layout(layout, niter=1000, start_temp=0.01, grid='nogrid')
     n = 1
     sz = len(ret_df.index)
@@ -165,7 +141,6 @@ def construct_filtered_graph(layout, df, country_label):
         igraph.plot(gg, layout=new_layout, target=tgt, bbox=(1600,900), margin=50)
         old_layout = new_layout.copy()
         n += 1
-
 
 
 @ray.remote
@@ -206,6 +181,7 @@ def generate_degree_timeseries(original_graph, df, ts_col):
 
     return ret_df
 
+
 def plot_and_save(df, filename, col_name):
     df_plot = df.plot()
     df_plot.set_xlabel('timestep')
@@ -214,6 +190,7 @@ def plot_and_save(df, filename, col_name):
     df_fig.savefig(filename)
 
 
+#Actual work starts here. Read configuration file
 cfg = load_config("config.ini")
 
 data_file = get_config(cfg, 'Data', 'data_file')
@@ -222,8 +199,6 @@ output_folder = get_config(cfg, 'Data', 'output_folder')
 ts_col = get_config(cfg, 'Simulation', 'timestep_col_name')
 start_ts = int(get_config(cfg, 'Simulation', 'start_timestep'))
 end_ts = int(get_config(cfg, 'Simulation', 'end_timestep'))
-bucket_sz = int(get_config(cfg, 'Simulation', 'bucket_size'))
-is_bucketed = True if get_config(cfg, 'Simulation', 'is_bucketed') == 'True' else False
 
 country_label = True if cfg['Format']['country_label'] == 'True' else False
 layout_name = cfg['Format']['layout_name']
@@ -233,40 +208,33 @@ Readin from : {data_file},
 timestep col: {ts_col},
 start_timestep: {start_ts},
 end_timestep: {end_ts},
-is_bucketed: {is_bucketed},
-bucket_sz: {bucket_sz}, and 
 writing to folder: {output_folder} 
 country_label: {country_label}
 '''
 print(str)
 
+
+#Load the dataframe
 cols_to_load = ['FromCty', 'ToCty']
 df, total_time = load_dataframe(data_file, ts_col, cols_to_load, end_ts)
-
 print(f'total simulation time is {total_time}')
 
+# check toggles on what to run
 run_animation = True if cfg['Workflow']['run_animation'] == 'True' else False
 run_betweenness = True if cfg['Workflow']['run_betweenness'] == 'True' else False
 run_degree = True if cfg['Workflow']['run_degree'] == 'True' else False
 
 
 if run_animation:
-    if is_bucketed:
-        bucketed_df= bucket_df(df, ts_col, bucket_sz)
-        construct_graph(layout_name, bucketed_df, ts_col, country_label)
-    else:
-        from_countries = set(G7 + BRICS)
-        ret_df = filter_dataframe(df, ts_col, from_countries, 8)
-        construct_filtered_graph(layout_name, ret_df, country_label)
+    from_countries = set(G7 + BRICS)
+    ret_df = filter_dataframe(df, ts_col, from_countries, 8)
+    original_graph = igraph.Graph.DataFrame(ret_df, directed=False, use_vids=False)
+    construct_graph(layout_name, ret_df, original_graph, country_label)
 
 
 if run_degree:
-#    filtered_df = filter_betweenness(df, 2018)
-#    filtered_df = filtered_df.reset_index(drop=True)
-#    filtered_df['timestep']=filtered_df.index
-#    print(len(filtered_df))
+    original_graph = igraph.Graph.DataFrame(df, directed=False, use_vids=False)
     degree_out_df = generate_degree_timeseries(original_graph, df, ts_col)
-    G7.append('China')
     g7_degree_out_df = degree_out_df[G7]
     g7_degree_out_df.to_csv('g7_degree.csv')
     out_file=f'{output_folder}/g7_degree.png'
@@ -284,10 +252,10 @@ if run_betweenness:
 #    filtered_df['timestep']=filtered_df.index
 #    print(len(filtered_df))
 #    between_df = generate_betweenness_timeseries(original_graph, filtered_df, ts_col)
+    original_graph = igraph.Graph.DataFrame(df, directed=False, use_vids=False)
     between_df = generate_betweenness_timeseries(original_graph, df, ts_col)
     between_df.to_csv('betweenness.csv')
 
-    G7.append('China')
     g7_betweenness_df = between_df[G7]
     g7_betweenness_df.to_csv('g7_betweenness.csv')
     out_file=f'{output_folder}/g7.png'
